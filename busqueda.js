88f7b49b67ba01da90522cc0ac2cc5c48547efbc
adgaol <37406034+adgaol@users.noreply.github.com>
2018-04-18 20:14:05 +0200

    Add files via upload
    lo mismo pero con comentarios

--- busqueda.js
+++ busqueda.js
@@ -2,8 +2,9 @@ $(function() {
     var boton = $("#boton");
     
    var listfoto= new Array();
-    
+    /*evento que se activa cuando el usuario busaca a partir de una fecha determinada */
     boton.click( dateMin);
+    /*funcion que busca a traves de flickr las imgenes subidas despues de fecha*/
     function dateMin(fecha){ 
         var fecha = $("#fechaMin").val();
         alert(fecha);
@@ -14,27 +15,30 @@ $(function() {
         );
     }
     
-
-function mostrar_fotos(info){
-	var i;
-	for (i=0;i<info.photos.photo.length;i++) {
-        if (!containe(info.photos.photo[i])){
+    /**
+     * muestras las fotos obtenidas de flickr
+     * @param info 
+     */
+    function mostrar_fotos(info){
+	    var i;
+	    for (i=0;i<info.photos.photo.length;i++) {
+            if (!containe(info.photos.photo[i])){
            
-	    var item =info.photos.photo[i];
-	    var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+	        var item =info.photos.photo[i];
+	        var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
 		          +'/'+item.id+'_'+item.secret+'_m.jpg';
-       console.debug(url);
+        console.debug(url);
        
-       $("#imagenes").append($("<img/>"+"<p>"+2018+"</p>").attr("src",url)); 
+        $("#imagenes").append($("<img/>"+"<p>"+2018+"</p>").attr("src",url)); 
         listfoto.push(info.photos.photo[i]);
+            }
         }
     }
-
-      
-    
-
-}
-function containe(foto){
+    /**
+     * comprueba si la foto ya estaba o se acaba de subir
+     *  
+     */
+    function containe(foto){
     var esta=false;
     var j;
     for(j=0;j<listfoto.length;j++){
