cffae0be9f39a268b41d4c75a641caecca421d1e
adgaol <37406034+adgaol@users.noreply.github.com>
2018-04-18 19:58:42 +0200

    Add files via upload
    ya busca pero se sigue repitiendo cada vez que pulsa el boton

--- busqueda.js
+++ busqueda.js
@@ -1,27 +1,47 @@
 $(function() {
     var boton = $("#boton");
-    var fecha = $("#fechaMin").value;
-    function dateMin(){ 
+    
+   var listfoto= new Array();
+    
+    boton.click( dateMin);
+    function dateMin(fecha){ 
+        var fecha = $("#fechaMin").val();
+        alert(fecha);
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
-	        + api_key + '&user_id=' +user_id + '&min_taken_date=' + fecha+
+	        + api_key + '&user_id=' +user_id + '&min_taken_date=' + fecha +
 	        '&format=json&nojsoncallback=1',
 	        mostrar_fotos
         );
     }
-    boton.click( dateMin);
+    
 
 function mostrar_fotos(info){
 	var i;
 	for (i=0;i<info.photos.photo.length;i++) {
-	   var item = info.photos.photo[i];
-	   var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
+        if (!containe(info.photos.photo[i])){
+           
+	    var item =info.photos.photo[i];
+	    var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
 		          +'/'+item.id+'_'+item.secret+'_m.jpg';
        console.debug(url);
        
-       $("#imagenes").append($("<img/>"+"<p>"+2018+"</p>").attr("src",url));
-      
+       $("#imagenes").append($("<img/>"+"<p>"+2018+"</p>").attr("src",url)); 
+        listfoto.push(info.photos.photo[i]);
+        }
     }
-}
 
+      
+    
+
+}
+function containe(foto){
+    var esta=false;
+    var j;
+    for(j=0;j<listfoto.length;j++){
+        if(foto==listfoto[j])
+            esta=true;
+    }
+    return esta;
+}
 }
 )	
--- credencialesFlikr.js
+++ credencialesFlikr.js
@@ -1,2 +1,2 @@
 var api_key ="baae9b0e79f9de91bd6d529c87b1aaa5";
-var user_id ="156539582@N05";
+var user_id =/*"3881302@N25"*/"156539582@N05";
