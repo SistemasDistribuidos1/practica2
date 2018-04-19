88dc3eba7bf3efba3f46be1856b920076b4027fe
adgaol <37406034+adgaol@users.noreply.github.com>
2018-04-19 18:48:20 +0200

    Add files via upload

--- busqueda.js
+++ busqueda.js
@@ -1,13 +1,14 @@
 $(function() {
    var boton = $("#boton");
-    
+    var fechaMin= $("#fechaMin");
    var listfoto= new Array();
     /*evento que se activa cuando el usuario busaca a partir de una fecha determinada */
     boton.click( dateMin);
+    fechaMin.
     /*funcion que busca a traves de flickr las imgenes subidas despues de fecha*/
     function dateMin(fecha){ 
         var fecha = $("#fechaMin").val();
-        alert(fecha);
+        $("#imagenes").append($("<p>"+fecha+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&min_taken_date=' + fecha +
 	        '&format=json&nojsoncallback=1',
@@ -20,7 +21,7 @@ $(function() {
     
     function tag(etiqueta){ 
         var etiqueta = $("#etiqueta1").val();
-        alert(etiqueta);
+        $("#imagenes").append($("<p>"+etiqueta+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&tags=' + etiqueta +
 	        '&format=json&nojsoncallback=1',
@@ -43,10 +44,22 @@ $(function() {
 		          +'/'+item.id+'_'+item.secret+'_m.jpg';
         console.debug(url);
        
-        $("#imagenes").append($("<img/>"+"<p>"+2018+"</p>").attr("src",url)); 
+        $("#imagenes").append($("<img/>").attr("src",url)); 
         listfoto.push(info.photos.photo[i]);
             }
         }
+    }
+    var botonPlace = $("#botonPlace");
+    botonPlace.click(place);
+    
+    function place(lugar){ 
+        var lugar = $("#lugar1").val();
+        alert(lugar);
+        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	        + api_key + '&user_id=' +user_id + '&has_geo=' + lugar +
+	        '&format=json&nojsoncallback=1',
+	        mostrar_fotos
+        );
     }
     /**
      * comprueba si la foto ya estaba o se acaba de subir
