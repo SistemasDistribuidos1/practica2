d2edcd9a9a05fa8c581204f93950477d07e5f429
adgaol <37406034+adgaol@users.noreply.github.com>
2018-04-20 20:48:49 +0200

    Add files via upload

--- busqueda.js
+++ busqueda.js
@@ -4,7 +4,7 @@ $(function() {
    var listfoto= new Array();
     /*evento que se activa cuando el usuario busaca a partir de una fecha determinada */
     boton.click( dateMin);
-    fechaMin.
+    
     /*funcion que busca a traves de flickr las imgenes subidas despues de fecha*/
     function dateMin(fecha){ 
         var fecha = $("#fechaMin").val();
@@ -15,7 +15,18 @@ $(function() {
 	        mostrar_fotos
         );
     }
-    
+    var botonMax = $("#botonMax");
+    botonMax.click(dateMax);
+    /*funcion que busca a traves de flickr las imagenes o videos subidos antes de fecha*/
+    function dateMax(fechaMax){ 
+        var fechaMax = $("#fechaMax1").val();
+        $("#imagenes").append($("<p>"+fechaMax+"</p>"));
+        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	        + api_key + '&user_id=' +user_id + '&max_taken_date=' + fechaMax +
+	        '&format=json&nojsoncallback=1',
+	        mostrar_fotos
+        );
+    }
     var botonTag = $("#botonTag");
     botonTag.click(tag);
     
@@ -30,6 +41,46 @@ $(function() {
     }
 
     
+
+    var botonPlace = $("#botonPlace");
+    botonPlace.click(place);
+    
+    function place(lugar){ 
+        var lugar = $("#lugar1").val();
+        $("#imagenes").append($("<p>"+lugar+"</p>"));
+        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	        + api_key + '&user_id=' +user_id + '&has_geo=' + lugar +
+	        '&format=json&nojsoncallback=1',
+	        mostrar_fotos
+        );
+    }
+
+    var botonMediaType = $("#botonMediaType");
+    botonMediaType.click(mediaType);
+    /*funcion que busca a traves de flickr las imagenes o videos subidos en función de su tipo, puediendo ser
+      all(todos por defecto), photos(fotos) o videos*/
+    function mediaType(tipo){ 
+        var tipo = $("#tipo1").val();
+        $("#imagenes").append($("<p>"+tipo+"</p>"));
+        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	        + api_key + '&user_id=' +user_id + '&media=' + tipo +
+	        '&format=json&nojsoncallback=1',
+	        mostrar_fotos
+        );
+    }
+    
+    var botonText = $("#botonText");
+    botonText.click(freeText);
+    /*funcion que busca fotos o videos cuyo titulo, etiqueta o descripcion contiene el texto buscado*/
+    function freeText(texto){ 
+        var texto = $("#texto1").val();
+        $("#imagenes").append($("<p>"+texto+"</p>"));
+        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
+	        + api_key + '&user_id=' +user_id + '&text=' + texto +
+	        '&format=json&nojsoncallback=1',
+	        mostrar_fotos
+        );
+    }
     /**
      * muestras las fotos obtenidas de flickr
      * @param info 
@@ -49,18 +100,7 @@ $(function() {
             }
         }
     }
-    var botonPlace = $("#botonPlace");
-    botonPlace.click(place);
-    
-    function place(lugar){ 
-        var lugar = $("#lugar1").val();
-        alert(lugar);
-        $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
-	        + api_key + '&user_id=' +user_id + '&has_geo=' + lugar +
-	        '&format=json&nojsoncallback=1',
-	        mostrar_fotos
-        );
-    }
+
     /**
      * comprueba si la foto ya estaba o se acaba de subir
      *  
