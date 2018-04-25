f0c44864bb8e542879c2f74b171b02f8ed8637bb
adgaol <37406034+adgaol@users.noreply.github.com>
2018-04-25 14:46:54 +0200

    Add files via upload

--- busqueda.js
+++ busqueda.js
@@ -9,10 +9,10 @@ $(function() {
     
     /*funcion que busca a traves de flickr las imgenes subidas despues de fecha*/
     function dateMin(fecha){ 
-
+        $("#imagenes #busq").remove();
         var fecha = $("#fechaMin").val();
        
-        $("#imagenes").append($("<p>"+fecha+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+fecha+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&min_taken_date=' + fecha +
 	        '&format=json&nojsoncallback=1',
@@ -25,8 +25,9 @@ $(function() {
     botonMax.click(dateMax);
     /*funcion que busca a traves de flickr las imagenes o videos subidos antes de fecha*/
     function dateMax(fechaMax){ 
+        $("#imagenes #busq").remove();
         var fechaMax = $("#fechaMax1").val();
-        $("#imagenes").append($("<p>"+fechaMax+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+fechaMax+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&max_taken_date=' + fechaMax +
 	        '&format=json&nojsoncallback=1',
@@ -39,8 +40,9 @@ $(function() {
     botonTag.click(tag);
     
     function tag(etiqueta){ 
+        $("#imagenes #busq").remove();
         var etiqueta = $("#etiqueta1").val();
-        $("#imagenes").append($("<p>"+etiqueta+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+etiqueta+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&tags=' + etiqueta +
 	        '&format=json&nojsoncallback=1',
@@ -54,8 +56,9 @@ $(function() {
     botonPlace.click(place);
     
     function place(lugar){ 
+        $("#imagenes #busq").remove();
         var lugar = $("#lugar1").val();
-        $("#imagenes").append($("<p>"+lugar+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+lugar+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&has_geo=' + lugar +
 	        '&format=json&nojsoncallback=1',
@@ -69,8 +72,9 @@ $(function() {
     /*funcion que busca a traves de flickr las imagenes o videos subidos en función de su tipo, puediendo ser
       all(todos por defecto), photos(fotos) o videos*/
     function mediaType(tipo){ 
+        $("#imagenes #busq").remove();
         var tipo = $("#tipo1").val();
-        $("#imagenes").append($("<p>"+tipo+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+tipo+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&media=' + tipo +
 	        '&format=json&nojsoncallback=1',
@@ -83,8 +87,9 @@ $(function() {
     botonText.click(freeText);
     /*funcion que busca fotos o videos cuyo titulo, etiqueta o descripcion contiene el texto buscado*/
     function freeText(texto){ 
+        $("#imagenes #busq").remove();
         var texto = $("#texto1").val();
-        $("#imagenes").append($("<p>"+texto+"</p>"));
+        $("#imagenes").append($("<p id=\"busq\">"+texto+"</p>"));
         $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=' 
 	        + api_key + '&user_id=' +user_id + '&text=' + texto +
 	        '&format=json&nojsoncallback=1',
@@ -98,25 +103,44 @@ $(function() {
      * @param info 
      */
     function mostrar_fotos(info){
-	    var i;
+        
+        
+        $("#imagenes #img").remove(); 
+        var i;
+        
 	    for (i=0;i<info.photos.photo.length;i++) {
 	        var item =info.photos.photo[i];
 	        var url = 'https://farm'+item.farm+".staticflickr.com/"+item.server
 		          +'/'+item.id+'_'+item.secret+'_m.jpg';
             console.debug(url);       
-            $("#imagenes").append($("<img id=\"img\"/>").attr("src",url));                         
+            $("#imagenes").append($("<img id=\"img\" style=width: 180px; height: 150px;\" width=\"180\" height=\"150\"/>").attr("src",url));                         
         }
-        $("#imagenes").append($("<br>"+"<img id=\"imagenGrande\"/>").attr("src",url)); 
+        $("#imagenes #img").click(zoom);
+         
     }
 
 
-    $("#imagenes > #img").click(zoom);
-    
+    /*Cambia el tamaño de la imagen*/
+    var nW,nH,oH,oW;
     function zoom(info){
-        alert(this.src());
-
-        $("#imagenGrande").attr("src",$(this).attr("src"));
+         var iWideSmall="180px";
+         var iHighSmall="150px";
+         var iWideLarge="540px";
+         var iHighLarge="450px";
+        
+        oW=this.style.width;
+        oH=this.style.height;
+        if((oW==iWideLarge)||(oH==iHighLarge)){ 
+            nW=iWideSmall;
+            nH=iHighSmall; }
+        else{ 
+            nW=iWideLarge;
+            nH=iHighLarge;
+            } 
+        this.style.width=nW;
+        this.style.height=nH;
     }
+
 
 }
 )	
